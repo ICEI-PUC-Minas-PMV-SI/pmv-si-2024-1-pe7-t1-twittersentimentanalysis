019aa7b9de06a9e31eee8bb080fe56ec27918a1a
WelbertJr <welbertjunior@live.com>
2024-05-13 19:38:33 -0300

    feat: add deploy configs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/pmv-si-2024-1-pe7-t1-twittersentimentanalysis",
+});
