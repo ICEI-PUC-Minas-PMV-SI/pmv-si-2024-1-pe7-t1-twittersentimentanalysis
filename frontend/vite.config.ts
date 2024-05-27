ba5f9506904f9581a57b24692643349a6aa59d1a
WelbertJr <welbertjunior@live.com>
2024-05-27 08:16:45 -0300

    config: deploy configs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/pmv-si-2024-1-pe7-t1-twittersentimentanalysis/",
+  base: "/pmv-si-2024-1-pe7-t1-twittersentimentanalysis",
 });
